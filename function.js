dc1b1c1003f3c808c5243b72f073b3e4ea87f196
ortizgrimm <danilobgomez@gmail.com>
2025-08-26 10:19:14 -0500

    modificando scrips

---
+++ function.js
@@ -0,0 +1,5 @@
+function CambiarColor(){
+            // alert("Hola Bienvenido");
+            document.body.style.backgroundColor = "Black";
+            document.getElementById("title").style.color = "White";
+        }
